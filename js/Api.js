e7592996428caa25d2106a1bd605891d23d9f999
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-10-10 11:23:07 +0200

    submit form works only in debugger not fixed

--- js/Api.js
+++ js/Api.js
@@ -1,4 +1,4 @@
-const uri = 'https://kadetten-dev.scapp.io/api/order';
+const uri = 'https://localhost:44389/api/order';
 
 
 
@@ -9,54 +9,38 @@ function deleteItem() {
 
 }
 function addItem(event) {
-  // var getfrom = document.getElementById("ticketform").elements;
-  // var items = document.getElementsByClassName("tickets");
-  // var tickets = [];
-  // var data = {
-  //   email: getfrom.namedItem("email").value,
-  //   clientLastName: getfrom.namedItem("prename").value,
-  //   clientFirstName: getfrom.namedItem("lastname").value,
-  //   bemerkung: getfrom.namedItem("text").value,
-  //   kadettLastName: getfrom.namedItem("child-lastname").value,
-  //   kadettFirstName: getfrom.namedItem("child-prename").value,
-  //   KadettInKader: getfrom.namedItem("child-kader").checked,
-  //   tickets: tickets
-  // }
-  // for (var i = 0; i < items.length; i++) {
-  //   if (items[i].value > 0) {
-  //     var ticket = {
-  //       type: items[i].getAttribute('data-ticket'),
-  //       quantity: Number(items[i].value),
-  //       day: items[i].getAttribute('data-day')
-  //     };
-  //     tickets.push(ticket);
-  //   }
-  // }
-  var data = {
-    email: "tuuut@gmail.com",
-    clientLastName: "tröt",
-    clientFirstName: "pipp",
-    bemerkung: "tröt",
-    kadettLastName: "nöbu",
-    kadettFirstName: "pipp",
-    tickets: [
-        {
-            type: "Erwachsene",
-            quantity: 10,
-            date: "Sa"
-        },
-        {
-            type: "Kind",
-            quantity: 5,
-            date: "So"
+    console.log("additem")
+    var getfrom = document.getElementById("ticketform").elements;
+    var items = document.getElementsByClassName("tickets");
+    var tickets = [];
+    var data = {
+      email: getfrom.namedItem("email").value,
+      clientLastName: getfrom.namedItem("prename").value,
+      clientFirstName: getfrom.namedItem("lastname").value,
+      bemerkung: getfrom.namedItem("text").value,
+      kadettLastName: getfrom.namedItem("child-lastname").value,
+      kadettFirstName: getfrom.namedItem("child-prename").value,
+      KadettInKader: getfrom.namedItem("child-kader").checked,
+      tickets: tickets
+    }
+    for (var i = 0; i < items.length; i++) {
+        var ticket = {
+          type: items[i].getAttribute('data-ticket'),
+          quantity: Number(items[i].value),
+          day: items[i].getAttribute('data-day')
+        };
+        tickets.push(ticket);
+    }
+    console.log(data);
+    fetch(uri, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
         }
-    ]
-}
-  console.log(data)
-  return fetch(uri, {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data)
-  }).then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.dir(err))
+    }).then(res => res.json())
+        .then(function(){
+            window.location.replace("/formfeedback.html");
+        })
+        .catch(error => console.error('Error:', error));
 }
