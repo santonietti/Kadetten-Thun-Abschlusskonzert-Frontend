c88269772fccd11cedc47a39a67e5962a81dcd4c
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-10-16 16:36:26 +0200

    redactor testing (post work, get is not deserialized)

--- js/redactor.js
+++ js/redactor.js
@@ -1,4 +1,6 @@
 // CREATE REDACTOR
+// const url = 'https://kadetten-dev.scapp.io/api/Redactor';
+const url = 'https://localhost:44389/api/Redactor';
 
 var toolbarOptions = [[{ 'header': [2, 3, false] }], ['bold'], ['link']];
 
@@ -8,3 +10,32 @@ var quill = new Quill('#editor', {
     toolbar: toolbarOptions
   }
 });
+
+function GetRedactor() {
+  fetch(url+"/intro")
+        .then(res => res.json())
+        .then(function (data) {
+    var p = document.createElement("p")
+    // p.innerHTML = data.text https://codepen.io/k3no/pen/amwpqk
+  });
+}
+function postRedactor() {
+  var about = document.querySelector('input[name=intro]');
+  about.value = JSON.stringify(quill.getContents());
+  var data = {
+    Name: about.getAttribute('data-redactor'),
+    Text: about.value
+  }
+  fetch(url, {
+    method: 'Put',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(function (myJson) {
+    console.log(myJson);
+  });
+}
+window.onload = function () {
+  GetRedactor();
+}
