5cbebdc02631a2402d2a6d9aa9862d244a228582
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-10-19 16:53:04 +0200

    In menu implemented

--- js/redactor.js
+++ js/redactor.js
@@ -116,6 +116,7 @@ function postFormularStatus() {
 	}).then(function (myJson) {
 		if (myJson.status == 200) {
 			button.setAttribute("data-status-active", status)
+			GetformularStatus();
 			if (status == true)
 				document.querySelector('#form-active-button > span').classList.add("active");
 			else if (status == false)
@@ -128,10 +129,16 @@ function GetformularStatus() {
 		.then(res => res.json())
 		.then(function (data) {
 			document.querySelector('#form-active-button').setAttribute("data-status-active", data)
-			if (data == true)
+			if (data == true) {
 				document.querySelector('#form-active-button > span').classList.add("active");
+				document.querySelector('#registration-live > span').classList.add("active")
+			}
 			else if (data == false)
+			{
 				document.querySelector('#form-active-button > span').classList.remove("active");
+				document.querySelector('#registration-live > span').classList.remove("active")
+			}
+
 		});
 
 }
